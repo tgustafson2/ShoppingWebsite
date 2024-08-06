a93cf05bad0a5fa25383086ba3e239184f986061
Thomas Gustafson <gus.duckfan@gmail.com>
2024-08-06 16:40:15 -0700

    Implements item card skeleton and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
+
 import './App.css'
+import ItemCard from './Components/ItemCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <ItemCard title="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops" price={109.95} imageUrl="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"/>
     </>
   )
 }
---
+++ src/Components/ItemCard.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Plus, Minus } from "lucide-react";
+import "./ItemCard.css"
+
+
+function ItemCard({title, price, imageUrl, addToCart}){
+    const [count, setCount] = useState(1);
+    const incrmentCount = () => {
+        if(isNaN(parseInt(count))){
+            setCount(1);
+        } else {
+            setCount(() => parseInt(count)+1);
+        }
+        
+    }
+    const decrmentCount = () =>{
+        if(isNaN(parseInt(count))){
+            setCount(1);
+        } else if(count>1) {
+            setCount(() => parseInt(count)-1);
+        }
+    }
+    const updateCount = (val) =>{
+        if((!isNaN(parseInt(val)) && parseInt(val)>0)||val===""){
+            setCount(val); 
+        }
+             
+    }
+
+    const checkIfAdd = () =>{
+        if(count!==""){
+            addToCart(title,price,count);
+            setCount(1);
+        }
+    }
+    return(<div className="item-card">
+        <div className="image-row">
+            <img src={imageUrl} />
+        </div>
+        <div className="item-info">
+            <div className="item-title">{title}</div>
+            <div className="item-price">{price}</div>
+        </div>
+        <div className="item-buy">
+            <button onClick={()=>incrmentCount()}><Plus /></button>
+            <input type="number" value={count} onChange={(e)=>updateCount(e.target.value)}></input>
+            <button onClick={()=>decrmentCount()}><Minus /></button>
+            <button onClick={()=>checkIfAdd()}>Add to cart</button>
+        </div>
+    </div>)
+}
+
+export default ItemCard;
